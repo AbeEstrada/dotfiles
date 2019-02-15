3fa3b8a1f9e0f55de01608c96d5fec5d91fff121
Abraham Estrada <abraham.estrada@gmail.com>
2019-02-15 12:02:26 -0700

    Remove old file from exclude

--- sync.sh
+++ sync.sh
@@ -2,5 +2,5 @@
 cd "$(dirname "$0")"
 rsync --exclude ".git/" --exclude ".gitignore" --exclude ".DS_Store" \
       --exclude "README.md" --exclude "LICENSE.md" --exclude "sync.sh" \
-      --exclude "Monokai Soda.terminal" --exclude "Preferences.sublime-settings" \
+      --exclude "Preferences.sublime-settings" \
       -avh . ~
