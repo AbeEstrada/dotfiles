fcb4e4e69af9a76f7071c90ea0c6b1b18de95ef4
Abe Estrada <abraham.estrada@gmail.com>
2024-09-03 13:28:54 -0600

    Nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -23,11 +23,50 @@ vim.opt.shiftwidth = 4
 -- Disable netrw
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+-- Set system clibpboard
+vim.opt.clipboard = "unnamedplus"
 
 -- Plugins:
 
 -- https://github.com/nvim-treesitter/nvim-treesitter
-require("nvim-treesitter").setup()
+-- require("nvim-treesitter").setup() 
+require("nvim-treesitter.configs").setup {
+	ensure_installed = {
+		"astro",
+		"bash",
+		"comment",
+		"css",
+		"csv",
+		"diff",
+		"editorconfig",
+		"gitignore",
+		"go",
+		"gomod",
+		"gosum",
+		"html",
+		"javascript",
+		"json",
+		"lua",
+		"rust",
+		"ruby",
+		"python",
+		"regex",
+		"toml",
+		"typescript",
+		"vim",
+		"xml",
+		"yaml",
+		"zig"
+	},
+	auto_install = false,
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = false,
+	},
+	indent = {
+		enable = true
+	}
+}
 
 -- https://github.com/nvim-tree/nvim-web-devicons
 require("nvim-web-devicons").setup()
@@ -35,23 +74,27 @@ require("nvim-web-devicons").setup()
 -- https://github.com/norcalli/nvim-colorizer.lua
 require("colorizer").setup()
 
+-- https://github.com/ggandor/leap.nvim
+-- require("leap").create_default_mappings()
+
 -- https://github.com/lukas-reineke/indent-blankline.nvim
 require("ibl").setup {
-	scope = {
-		enabled = true,
-		show_start = true,
-		show_end = true
+	indent = {
+		char = "▏",
 	},
+	scope = {
+		show_exact_scope = true,
+	}
 }
 
 -- https://github.com/akinsho/bufferline.nvim
 require("bufferline").setup {
 	options = {
-		show_buffer_icons = false,
 		right_mouse_command = false,
+		show_buffer_icons = false,
 		indicator = {
-			style = "underline",
-		},
+			style = "underline"
+		}
 	}
 }
 
@@ -68,9 +111,12 @@ require("nvim-tree").setup()
 vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { silent = true })
 
 -- https://github.com/nvim-telescope/telescope.nvim
+-- https://github.com/nvim-telescope/telescope-file-browser.nvim
 require("telescope").setup()
+require("telescope").load_extension("fzf") -- make
 local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-	vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+-- vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+-- vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+vim.keymap.set("n", "<space>fe", ":Telescope file_browser<CR>")
