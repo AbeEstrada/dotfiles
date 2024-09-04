01c3e60b240fe1b19500aeb9678e27ed29cea74d
Abe Estrada <abraham.estrada@gmail.com>
2024-09-04 09:51:01 -0600

    Nvim: which-key

--- nvim/init.lua
+++ nvim/init.lua
@@ -29,7 +29,7 @@ vim.opt.clipboard = "unnamedplus"
 -- Plugins:
 
 -- https://github.com/nvim-treesitter/nvim-treesitter
--- require("nvim-treesitter").setup() 
+-- require("nvim-treesitter").setup()
 require("nvim-treesitter.configs").setup {
 	ensure_installed = {
 		"astro",
@@ -45,27 +45,43 @@ require("nvim-treesitter.configs").setup {
 		"gosum",
 		"html",
 		"javascript",
+		"jsdoc",
 		"json",
+		"jsonc",
 		"lua",
+		"markdown",
+		"markdown_inline",
 		"rust",
 		"ruby",
 		"python",
 		"regex",
 		"toml",
+		"tsx",
 		"typescript",
 		"vim",
+		"vimdoc",
 		"xml",
 		"yaml",
 		"zig"
 	},
 	auto_install = false,
+	indent = { enable = true },
 	highlight = {
 		enable = true,
 		additional_vim_regex_highlighting = false,
 	},
 	indent = {
 		enable = true
-	}
+	},
+    textobjects = {
+      move = {
+        enable = true,
+        goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer", ["]a"] = "@parameter.inner" },
+        goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
+        goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer", ["[a"] = "@parameter.inner" },
+        goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer", ["[A"] = "@parameter.inner" },
+      },
+    }
 }
 
 -- https://github.com/windwp/nvim-ts-autotag
@@ -82,6 +98,12 @@ require("colorizer").setup()
 
 -- https://github.com/ggandor/leap.nvim
 require("leap").create_default_mappings()
+
+-- https://github.com/chrisgrieser/nvim-spider
+require("spider").setup()
+vim.keymap.set({ "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<CR>", { desc = "Spider-w" })
+vim.keymap.set({ "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<CR>", { desc = "Spider-e" })
+vim.keymap.set({ "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>", { desc = "Spider-b" })
 
 -- https://github.com/stevearc/dressing.nvim
 require("dressing").setup()
@@ -123,17 +145,24 @@ require("gitsigns").setup()
 
 -- https://github.com/nvim-tree/nvim-tree.lua
 require("nvim-tree").setup()
-vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { silent = true })
+vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { silent = true, desc = " Tree Toggle" })
 
 -- https://github.com/nvim-telescope/telescope.nvim
--- https://github.com/nvim-telescope/telescope-file-browser.nvim
--- https://github.com/nvim-telescope/telescope-ui-select.nvim
 require("telescope").setup()
+-- https://github.com/nvim-telescope/telescope-file-browser.nvim
 require("telescope").load_extension("fzf") -- make
+-- https://github.com/nvim-telescope/telescope-ui-select.nvim
 require("telescope").load_extension("ui-select")
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
-vim.keymap.set("n", "<space>fe", ":Telescope file_browser<CR>")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
+vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live Grep" })
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help Tags" })
+vim.keymap.set("n", "<leader>fe", ":Telescope file_browser<CR>", { desc = "File Browser" })
+
+-- https://github.com/folke/which-key.nvim
+require("which-key").setup {
+	opts = {
+		preset = "helix"
+	}
+}
