3e85cd32c108de63247af911c35e397490978acf
Abe Estrada <abraham.estrada@gmail.com>
2025-07-20 00:54:49 -0600

    nvim v0.12

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,15 +1,29 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
+local o = vim.opt
+o.number = true
+o.tabstop = 2
+o.shiftwidth = 2
+o.softtabstop = 2
+o.expandtab = true
 
-require("vim-opt")
-require("lazy").setup("plugins")
+local g = vim.g
+g.mapleader       = " "
+g.maplocalleader  = " "
+
+vim.pack.add({
+  "https://github.com/folke/tokyonight.nvim",
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
+})
+
+require("tokyonight.colors").setup()
+vim.cmd[[colorscheme tokyonight-moon]]
+
+local ts_parsers = {
+  "lua"
+}
+local nts = require("nvim-treesitter")
+nts.install(ts_parsers)
+nts.update()
+
+vim.keymap.set("n", "<leader>r", function()
+  vim.cmd("luafile ~/.config/nvim/init.lua")
+end, { desc = "Reload Neovim config" })
