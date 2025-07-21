96846d13f628faaeecc031a27b57587a82a507c2
Abe Estrada <abraham.estrada@gmail.com>
2025-07-20 23:26:33 -0600

    nvim more plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,9 +12,11 @@ g.maplocalleader  = " "
 
 vim.pack.add({
   "https://github.com/folke/tokyonight.nvim",
-  "https://github.com/nvim-lualine/lualine.nvim",
   "https://github.com/akinsho/bufferline.nvim",
-  { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
+  "https://github.com/nvim-lualine/lualine.nvim",
+  "https://github.com/folke/flash.nvim",
+  "https://github.com/nvim-treesitter/nvim-treesitter",
+  "https://github.com/nvim-treesitter/nvim-treesitter-context",
 })
 
 require("tokyonight.colors").setup()
@@ -24,7 +26,14 @@ require("lualine").setup {
   options = { section_separators = "", component_separators = "" }
 }
 
-require("bufferline").setup()
+require("bufferline").setup {
+  options = {
+    show_buffer_icons = false,
+    indicator = {
+      style = "underline",
+    },
+  }
+}
 
 local ts_parsers = {
   "lua"
@@ -32,4 +41,13 @@ local ts_parsers = {
 local nts = require("nvim-treesitter")
 nts.install(ts_parsers)
 nts.update()
+
+require("treesitter-context").setup()
+
+local flash = require("flash")
+vim.keymap.set({"n", "x", "o"}, "s", function() flash.jump() end, {desc = "Flash"})
+vim.keymap.set({"n", "x", "o"}, "S", function() flash.treesitter() end, {desc = "Flash Treesitter"})
+vim.keymap.set("o", "r", function() flash.remote() end, {desc = "Remote Flash"})
+vim.keymap.set({"o", "x"}, "R", function() flash.treesitter_search() end, {desc = "Treesitter Search"})
+vim.keymap.set("c", "<c-s>", function() flash.toggle() end, {desc = "Toggle Flash Search"})
 
