e7bf0ac9aa084add166fd8e1636313f2a120f9f4
Abe Estrada <abraham.estrada@gmail.com>
2025-07-20 19:30:44 -0600

    neovim

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,6 +4,7 @@ o.tabstop = 2
 o.shiftwidth = 2
 o.softtabstop = 2
 o.expandtab = true
+o.termguicolors = true
 
 local g = vim.g
 g.mapleader       = " "
@@ -11,11 +12,19 @@ g.maplocalleader  = " "
 
 vim.pack.add({
   "https://github.com/folke/tokyonight.nvim",
+  "https://github.com/nvim-lualine/lualine.nvim",
+  "https://github.com/akinsho/bufferline.nvim",
   { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
 })
 
 require("tokyonight.colors").setup()
 vim.cmd[[colorscheme tokyonight-moon]]
+
+require("lualine").setup {
+  options = { section_separators = "", component_separators = "" }
+}
+
+require("bufferline").setup()
 
 local ts_parsers = {
   "lua"
@@ -24,6 +33,3 @@ local nts = require("nvim-treesitter")
 nts.install(ts_parsers)
 nts.update()
 
-vim.keymap.set("n", "<leader>r", function()
-  vim.cmd("luafile ~/.config/nvim/init.lua")
-end, { desc = "Reload Neovim config" })
